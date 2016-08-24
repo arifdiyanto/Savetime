55fe0684b31743936e9d5534ba9adafc75b3d2d3
arifdiyanto <arifdiyantotmg@gmail.com>
2016-08-24 16:54:55 +0700

    Change caption app

--- view/layouts/adminlte.php
+++ view/layouts/adminlte.php
@@ -16,9 +16,9 @@
             <!-- Logo -->
             <a href="<?=ROOT?>" class="logo">
                 <!-- mini logo for sidebar mini 50x50 pixels -->
-                <span class="logo-mini"><b>SF</b></span>
+                <span class="logo-mini"><b>StF</b></span>
                 <!-- logo for regular state and mobile devices -->
-                <span class="logo-lg"><b>SF</b> Demo</span>
+                <span class="logo-lg"><b>Save</b> Time</span>
             </a>
             <!-- Header Navbar: style can be found in header.less -->
             <nav class="navbar navbar-static-top">
